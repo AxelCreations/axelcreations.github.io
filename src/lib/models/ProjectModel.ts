e5df172ac83c172ef4a5efb0117ac9e92030e59e
Reynel Axel Perdomo <ReynelAxel@gmail.com>
2024-01-18 15:43:32 -0400

    Added projects data

--- src/lib/models/ProjectModel.ts
+++ src/lib/models/ProjectModel.ts
@@ -2,7 +2,11 @@ import SkillModel from './SkillModel';
 
 type ProjectModel = {
   title: string;
+  company: string;
   description: string;
+  date: string;
+  link: string | null;
+  repo: string | null;
   images: string[];
   skills: SkillModel[] | null;
 }
