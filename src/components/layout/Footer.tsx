9fd6f0a8496024a250ffdbbb04deed1bfb9b901a
Reynel Axel Perdomo <ReynelAxel@gmail.com>
2024-01-23 23:52:53 -0400

    fixed footer image

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <StyledFooter className="App-header bg-secondary">
       <StyledContainer>
         <div className="logo">
-          <img src={logo} height={110} width={110} alt='Reynel Axel Logo' />
+          <img src={logo} height={150} width={150} alt='Reynel Axel Logo' />
         </div>
       </StyledContainer>
     </StyledFooter>
@@ -19,14 +19,22 @@ export default Footer;
 const StyledContainer = styled(Container)`
   display: flex;
   justify-content: center;
-`
+  padding-top: 60px;
+  padding-bottom: 60px;
+  overflow: hidden;
+  
+  .logo {
+    img {
+      @media screen and (max-width: 768px) {
+        height: 120px;
+        width: 120px;
+      }
+    }
+  }
+  `;
 
 const StyledFooter = styled.footer`
+  overflow: hidden;
   align-items: center;
   display: flex;
-  height: 150px;
-
-  @media screen and (max-width: 768px) {
-    height: 120px;
-  }
 `
