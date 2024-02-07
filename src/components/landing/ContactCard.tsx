f34107a5fa78bab3a1efe28400b1e21f141c7ce2
Reynel Axel Perdomo <ReynelAxel@gmail.com>
2024-02-06 20:38:24 -0400

    New contacts style

--- src/components/landing/ContactCard.tsx
+++ src/components/landing/ContactCard.tsx
@@ -10,7 +10,7 @@ const ContactCard = ({ contact }: ContactProps) => {
   return (
     <StyledDiv href={contact.link} target="_blank">
       <div className="icon">
-        <Img src={contact.icon} text={`Contact ${contact.text}`} height={85} showPlaceholder={false} />
+        <Img src={contact.icon} text={`Contact ${contact.text}`} height={32} showPlaceholder={false} />
       </div>
       <div className="text">
         <span>{contact.text}</span>
@@ -23,34 +23,41 @@ export default ContactCard;
 
 const StyledDiv = styled.a`
   align-items: center;
-  background-color: var(--bg-secondary);
-  color: var(--color-white-smoke);
+  background-color: var(--bg-primary);
+  color: var(--color-black);
   display: flex;
+  border-radius: 24px;
   gap: 1rem;
+  padding: 1rem 0;
+  border: 6px solid var(--color-black);
+  justify-content: center;
   text-decoration: none;
   
   &:hover {
+  background-color: var(--color-white-smoke);
     box-shadow: 0 0 6px 2px rgba(0, 0, 0, 0.125);
-    color: var(--color-white);
+    color: var(--color-black);
   }
   
   .icon {
+    align-items: center;
     display: flex;
-    height: 85px;
-    width: 85px;
+    height: 50px;
+    justify-content: center;
+    width: 50px;
     
     @media screen and (max-width: 1200px) {
-      height: 70px;
-      width: 70px;
+      height: 40px;
+      width: 40px;
     }
 
     img {
-      max-height: 85px;
-      max-width: 85px;
+      max-height: 32px;
+      max-width: 32px;
     
       @media screen and (max-width: 1200px) {
-        height: 70px;
-        width: 70px;
+        height: 28px;
+        width: 28px;
       }
     }
   }
